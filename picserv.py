efd3a44b44959a9b6cdf33b904e6c424318d0adf
ngrande <devlup@outlook.com>
2016-02-12 11:23:15 +0100

    found / implemented a workaround

--- picserv.py
+++ picserv.py
@@ -5,6 +5,7 @@ import _thread
 import sys
 import os
 import string
+import time
 
 
 class PicServ:
@@ -20,23 +21,32 @@ class PicServ:
     def start(self):
         self.serversocket.bind(('', self.port))
         self.serversocket.listen(5)
-
         while True:
             (clientsocket, address) = self.serversocket.accept()
+            _thread.start_new_thread(self._send, (clientsocket,))
+
+    def _send(self, socket):
             print('client accepted on port {0!s}'.format(self.port))
-            with open(self.data_path, 'r') as f:
-                content = f.read()
-                contentsize = len(content)
-                clientsocket.send(bytes('HTTP/1.1 200 OK', 'utf-8'))
-                clientsocket.send(bytes('Content-type: text/html;'
-                                        'charset=utf-8', 'utf-8'))
-                clientsocket.send(bytes('Accept-ranges: bytes', 'utf-8'))
-                clientsocket.send(bytes('Content-length: {0!s}'
-                                        .format(contentsize), 'utf-8'))
-                sent = 0
-                while sent < contentsize:
-                    sent += clientsocket.send((bytes(content[sent:], 'utf-8')))
-            clientsocket.close()
+            with open(self.data_path, 'rb') as f:
+
+                # content = f.read()
+                contentsize = os.path.getsize(self.data_path)
+                # content = f.read()
+                # contentsize = len(content)
+                # socket.send(bytes('HTTP/1.1 200 OK', 'utf-8'))
+                # socket.send(bytes('Content-type: image/gif;', 'utf-8'))
+                #                         'charset=utf-8', 'utf-8'))
+                # socket.send(bytes('Accept-ranges: bytes', 'utf-8'))
+                # socket.send(bytes('Content-length: {0!s}'
+                #                   .format(contentsize), 'utf-8'))
+                # totalsent = 0
+                # while totalsent < contentsize:
+                socket.sendfile(f)
+                #   if sent == 0:
+                #       print('Client closed connection')
+                #   totalsent += sent
+            time.sleep(0.01)  # workaround... TODO: find better solution
+            socket.close()
             print('removed client on port {0!s}'.format(self.port))
 
 
