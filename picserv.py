bcc4314cef0b20176180e9e487435fcc996cc308
ngrande <devlup@outlook.com>
2016-02-12 18:18:29 +0100

    small bugfixes

--- picserv.py
+++ picserv.py
@@ -21,17 +21,25 @@ class PicServ:
     def start_async(self):
         _thread.start_new_thread(self.start, ())
 
+    def stop(self):
+        self.active = False
+        self.server_socket.close()
+
     def start(self):
         self.server_socket.bind((self.addr, self.port))
         self.server_socket.listen(5)
+        self.active = True
         print('PicServ instance started on  {0!s}:{1!s}'.format(self.addr,
                                                                 self.port))
-        while True:
-            (client_socket, address) = self.server_socket.accept()
-            print('client connected on port {0!s}, starting new thread'
-                  .format(self.port))
-            _thread.start_new_thread(self._process_request, (client_socket,
-                                                             address))
+        try:
+            while self.active:
+                (client_socket, address) = self.server_socket.accept()
+                print('client connected on port {0!s}, starting new thread'
+                      .format(self.port))
+                _thread.start_new_thread(self._process_request, (client_socket,
+                                                                 address))
+        finally:
+            self.server_socket.close()
 
     def _process_request(self, socket, address):
         print('client connected on {0!s}:{1!s}'.format(address, self.port))
@@ -46,8 +54,8 @@ class PicServ:
                 # get requested resource path from msg
                 print('GET request received')
                 req_source_path = self._get_path(msg)
-                if self._exists_resource(req_source_path)
-                or req_source_path == '/':
+                if (self._exists_resource(req_source_path) or
+                        req_source_path == '/'):
                     print('requested resource:')
                     print(req_source_path)
                     file_bytes = self._get_res_file_bytes(req_source_path)
@@ -86,7 +94,7 @@ class PicServ:
         # while len(recv) != 0 and recv != 0:
         # TODO: check if there is a message to be received by the host...
         # else the server will wait forever
-        recv = socket.recv(2048)  # TODO: find a way to be sure that all bytes
+        recv = socket.recv(4096)  # TODO: find a way to be sure that all bytes
         #                                 were received
         #   print(recv)
         #   recvbytes += recv
@@ -107,10 +115,16 @@ class PicServ:
 def main():
     if len(sys.argv) < 4:
         print('At least 3 arguments were expected: root_path, addr and port')
-    for port in sys.argv[3:]:
-        ps = PicServ(sys.argv[2], int(port), sys.argv[1])
-        ps.start_async()
-    input()
+    servers = []
+    try:
+        for port in sys.argv[3:]:
+            ps = PicServ(sys.argv[2], int(port), sys.argv[1])
+            ps.start_async()
+            servers.append(ps)
+        input()
+    finally:  # be sure to stop the servers
+        for s in servers:
+            s.stop()
 
 
 if __name__ == "__main__":
